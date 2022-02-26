0df88c29c15b20f0a0e08a874cc3ec4d3b0467a8
S K <65646599+l-SK-l@users.noreply.github.com>
2022-02-26 19:39:59 +0300

    master

--- Задачи_1.py
+++ Задачи_1.py
@@ -1,4 +1,20 @@
+#1. Любимые вещи
+from email import message
+from re import S
+from tkinter.font import families
+
+
 games = ['chees', 'football']
 foods = ['apple', 'banana']
 favorites = (games + foods)
 print(favorites)
+#2. Подсчет воинов
+samuraes = int(3 * 25)
+tunnels = int(2 * 40)
+warriors = samuraes + tunnels
+print(warriors)
+#3. Приветствие
+name = 'Sergey'
+surname = 'Kozlov'
+message = 'Привет, %s %s!'
+print(message % (name, surname) )
