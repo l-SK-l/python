d8c9553cf4487d52ba136a4419a96017be0bd820
S K <65646599+l-SK-l@users.noreply.github.com>
2022-03-02 10:58:26 +0300

    master

--- Задачи_1.py
+++ Задачи_1.py
@@ -18,3 +18,8 @@ name = 'Sergey'
 surname = 'Kozlov'
 message = 'Привет, %s %s!'
 print(message % (name, surname) )
+
+# Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!)
+n = int(input())
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
