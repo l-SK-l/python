b6d52b912fb0e9b73626e157559e80374a3ea315
S K <65646599+l-SK-l@users.noreply.github.com>
2022-03-27 22:10:24 +0300

    master

--- 1.py
+++ 1.py
@@ -1,6 +1,7 @@
-def spaceship_building(cans):
-    total_cans = 0 
-    for week in range(1, 53):
-        total_cans = total_cans + cans
-        print('Неделя %s, банок: %s' % (week, total_cans))
-spaceship_building(2)
+a = ['first', 1, 2, 3, 'second', 10, 20, 'third', 15, 56, 70, 'fourth', -50]
+b = {}
+for key, value in a():
+    if type(key) == str:
+        b[key] = []
+        
+print(b)
